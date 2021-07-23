0791c5abe56341df84e31a93779304d5fe2a0840
Szymon Steczek <szymon.steczek42@gmail.com>
2021-07-23 07:50:54 +0200

    Add database models.

--- dj_backend_server/dj_backend_server/settings.py
+++ dj_backend_server/dj_backend_server/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'debug_toolbar'
+    'debug_toolbar',
+    'driver_car_app',
 ]
 
 MIDDLEWARE = [
