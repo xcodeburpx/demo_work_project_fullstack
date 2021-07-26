e54c4e28e900432031b02c44b49dbd85243fec29
Szymon Steczek <szymon.steczek42@gmail.com>
2021-07-26 21:12:25 +0200

    Create REST method to extract car newest GPS entry

--- dj_backend_server/dj_backend_server/urls.py
+++ dj_backend_server/dj_backend_server/urls.py
@@ -18,14 +18,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-from rest_framework import routers
-from driver_car_app import views
 
-router = routers.DefaultRouter()
-router.register(r'gps', views.GpsViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path("", include("driver_car_app.urls"), name="car_gps_records"),
     path('admin/', admin.site.urls),
     path("__debug__", include(debug_toolbar.urls)),
 ]
--- dj_backend_server/driver_car_app/views.py
+++ dj_backend_server/driver_car_app/views.py
@@ -1,11 +1,25 @@
+from django.db import reset_queries
 from .models import Gps
-from rest_framework import viewsets
-from rest_framework import permissions
+from rest_framework import viewsets, permissions, generics, status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from .serializers import GpsSerializer
-from django.shortcuts import render
 
 # Create your views here.
-
-class GpsViewSet(viewsets.ModelViewSet):
+class GpsListView(generics.ListAPIView):
     queryset = Gps.objects.all().order_by('timestamp')
     serializer_class = GpsSerializer
+
+
+@api_view(["GET",])
+def api_get_newest_location(request, truck_name):
+    try:
+        gps = Gps.objects.filter(truck__truck_name=truck_name).order_by("timestamp").first()
+        if gps is None:
+            raise Gps.DoesNotExist()
+    except Gps.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == "GET":
+        serializer = GpsSerializer(gps)
+        return Response(serializer.data)
